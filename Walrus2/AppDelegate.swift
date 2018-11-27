69aea70c40b654df9083a9a9b9c9f9009b1c32a9
au2001 <au2001sfg@gmail.com>
2018-11-27 23:21:23 +0100

    Initial Commit

---
+++ Walrus2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Walrus2
+//
+//  Created by Aurélien on 27/11/2018.
+//  Copyright © 2018 Walrus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
