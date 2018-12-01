99fe31fb94561a46793c882374a78d42e90718cd
au2001 <au2001sfg@gmail.com>
2018-12-01 17:42:03 +0100

    Fix moving diagonally

--- Walrus/API/Controls.swift
+++ Walrus/API/Controls.swift
@@ -62,6 +62,34 @@ class Controls {
         if moveX != 0 && moveY != 0 {
             moveX /= SQRT_2
             moveY /= SQRT_2
+
+            let rx = (game.player.x - game.player.x.rounded(.down)).truncatingRemainder(dividingBy: 1)
+            let ry = (game.player.y - game.player.y.rounded(.down)).truncatingRemainder(dividingBy: 1)
+
+            let nx = rx + moveX * SPEED * delta
+            let ny = ry + moveY * SPEED * delta
+
+            if (nx < 0 || nx > 1) != (ny < 0 || ny > 1) {
+                if (nx < 0 || nx > 1) && !(ny < 0 || ny > 1) {
+                    if (moveX > 0) == (moveY > 0) {
+                        // rx + moveX * SPEED * delta = ry + moveY * SPEED * delta
+                        // moveX * SPEED * delta = ry - rx + moveY * SPEED * delta
+                        // moveX = ry - rx + moveY
+                        moveX = ry - rx + moveY
+                    } else {
+                        // rx + moveX * SPEED * delta = ry - moveY * SPEED * delta
+                        // moveX * SPEED * delta = ry - rx- moveY * SPEED * delta
+                        // moveX = ry - rx - moveY
+                        moveX = ry - rx - moveY
+                    }
+                } else if (ny < 0 || ny > 1) && !(nx < 0 || nx > 1) {
+                    if (moveX > 0) == (moveY > 0) {
+                        moveY = rx - ry + moveX
+                    } else {
+                        moveY = rx - ry - moveX
+                    }
+                }
+            }
         }
 
         synchronized(game.player) {
