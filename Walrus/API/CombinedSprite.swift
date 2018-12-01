0db1709e2bc151c7cf728157bfb8f1602f078147
au2001 <au2001sfg@gmail.com>
2018-12-01 14:19:29 +0100

    Create combined sprites

---
+++ Walrus/API/CombinedSprite.swift
@@ -0,0 +1,98 @@
+//
+//  CombinedSprite.swift
+//  Walrus
+//
+//  Created by Aurélien on 01/12/2018.
+//  Copyright © 2018 Walrus. All rights reserved.
+//
+
+import Cocoa
+
+class CombinedSprite: Sprite {
+
+    var sprites: [(sprite: Sprite, position: Pixel)]
+
+    init(sprites: [(sprite: Sprite, position: Pixel)]) {
+        self.sprites = sprites
+
+        super.init(pixels: [], anchorX: 0, anchorY: 0)
+    }
+
+    func add(sprite: Sprite, at position: Pixel) {
+        sprites.append((sprite: sprite, position: position))
+    }
+
+    override func getWidth() -> Int {
+        var minX = 0, maxX = 0
+        for (sprite, position) in self.sprites {
+            minX = min(sprite.getMinX() + position.x, minX)
+            maxX = max(sprite.getMaxX() + position.x, maxX)
+        }
+        return maxX - minX
+    }
+
+    override func getHeight() -> Int {
+        var minY = 0, maxY = 0
+        for (sprite, position) in self.sprites {
+            minY = min(sprite.getMinY() + position.y, minY)
+            maxY = max(sprite.getMaxY() + position.y, maxY)
+        }
+        return maxY - minY
+    }
+
+    override func getMinX() -> Int {
+        var minX = 0
+        for (sprite, position) in self.sprites {
+            minX = min(sprite.getMinX() + position.x, minX)
+        }
+        return minX
+    }
+
+    override func getMinY() -> Int {
+        var minY = 0
+        for (sprite, position) in self.sprites {
+            minY = min(sprite.getMinY() + position.y, minY)
+        }
+        return minY
+    }
+
+    override func getMaxX() -> Int {
+        var maxX = 0
+        for (sprite, position) in self.sprites {
+            maxX = max(sprite.getMaxX() + position.x, maxX)
+        }
+        return maxX
+    }
+
+    override func getMaxY() -> Int {
+        var maxY = 0
+        for (sprite, position) in self.sprites {
+            maxY = max(sprite.getMaxY() + position.y, maxY)
+        }
+        return maxY
+    }
+
+    override func getColor(x: Int, y: Int) -> CGColor {
+        var color = CGColor.clear
+
+        for (sprite, position) in self.sprites {
+            color = ContentView.blend(color: sprite.getColor(x: x - position.x, y: y - position.y), above: color)
+        }
+
+        return color
+    }
+
+    override func getHitBox(threshold: Double = 0.5) -> Set<Pixel> {
+        var hitbox: Set<Pixel> = []
+
+        for (sprite, position) in self.sprites {
+            for pixel in sprite.getHitBox(threshold: threshold) {
+                hitbox.insert(Pixel(x: position.x + pixel.x, y: position.y + pixel.y))
+            }
+        }
+
+        return hitbox
+    }
+
+}
+
